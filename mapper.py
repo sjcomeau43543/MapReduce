e10a96b91cac6e87ad5df509d3391187f3c3a169
Samantha Comeau <sjcomeau@wpi.edu>
2019-01-17 16:03:59 -0500

    mapper and reducer

---
+++ mapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+
+def main():
+	for line in sys.stdin:
+	    # remove leading and trailing whitespace
+	    line = line.strip()
+
+	    words = line.split()
+
+	    for word in words:
+		# write output to the stdout so the reducer can get it
+		# tab delimited
+		print '%s\t%s' % (word, 1)
+
+if __name__ == "__main__":
+	main()
